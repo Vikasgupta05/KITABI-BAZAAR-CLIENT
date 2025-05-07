45936e9233217aa31bb76454db35a23cfefc8471
Vikasgupta05 <vikasnwrdr2001@gmail.com>
2025-05-07 16:43:45 +0530

    create navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Dashboard } from './Pages/deshboard'
+import "./App.css"
+function App() {
+
+  return (
+    <>
+      <div>
+          <Dashboard></Dashboard>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Deshboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavBar } from "../Component/Navbar";
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  );
+};
